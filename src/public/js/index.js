5715e538f88ce583f77315db30185dea78c9063f
Lajos Seyler <lajos.seyler@gmail.com>
2022-01-09 14:07:28 -0500

    Switched from Boostrap to Tailwind CSS. Redesigned login page. Removed extra code from loggedin page to start from scratch.

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -14,14 +14,6 @@
     return hashParams;
   }
 
-  var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-      userProfileTemplate = Handlebars.compile(userProfileSource),
-      userProfilePlaceholder = document.getElementById('user-profile');
-
-  var oauthSource = document.getElementById('oauth-template').innerHTML,
-      oauthTemplate = Handlebars.compile(oauthSource),
-      oauthPlaceholder = document.getElementById('oauth');
-
   var params = getHashParams();
 
   var access_token = params.access_token,
@@ -32,19 +24,14 @@
     alert('There was an error during the authentication');
   } else {
     if (access_token) {
-      // render oauth info
-      oauthPlaceholder.innerHTML = oauthTemplate({
-        access_token: access_token,
-        refresh_token: refresh_token
-      });
-
       $.ajax({
           url: 'https://api.spotify.com/v1/me',
           headers: {
             'Authorization': 'Bearer ' + access_token
           },
           success: function(response) {
-            userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+            document.getElementById("display_name").innerText = response.display_name;
+            document.getElementById("user_id").innerText = response.id;
 
             $('#login').hide();
             $('#loggedin').show();
@@ -56,19 +43,5 @@
         $('#loggedin').hide();
     }
 
-    document.getElementById('obtain-new-token').addEventListener('click', function() {
-      $.ajax({
-        url: '/refresh_token',
-        data: {
-          'refresh_token': refresh_token
-        }
-      }).done(function(data) {
-        access_token = data.access_token;
-        oauthPlaceholder.innerHTML = oauthTemplate({
-          access_token: access_token,
-          refresh_token: refresh_token
-        });
-      });
-    }, false);
   }
 })();
